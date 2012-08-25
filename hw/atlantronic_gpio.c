d28d86604a40c403808f4cabc3ed59221356c460
Trédez Jean-Baptiste <jbtredez@gmail.com>
2012-08-25 16:56:01 +0200

    rangement des modules usb, rcc dans des fichiers + ajout module gpio

---
+++ hw/atlantronic_gpio.c
@@ -0,0 +1,163 @@
+#define LINUX
+#include "kernel/cpu/cpu.h"
+#undef LINUX
+#include "kernel/driver/usb/otgd_fs_regs.h"
+
+#include "sysbus.h"
+#include "arm-misc.h"
+#include "boards.h"
+#include "exec-memory.h"
+
+#define PIN_NUM        8
+
+struct atlantronic_gpio_state
+{
+	SysBusDevice busdev;
+	MemoryRegion iomem;
+	GPIO_TypeDef gpio;
+	qemu_irq irq[PIN_NUM];
+};
+
+static void atlantronic_gpio_write(void *opaque, target_phys_addr_t offset, uint64_t val, unsigned size)
+{
+	struct atlantronic_gpio_state* s = opaque;
+
+	switch(offset)
+	{
+		case offsetof(GPIO_TypeDef, CRL):
+			s->gpio.CRL = val;
+			break;
+		case offsetof(GPIO_TypeDef, CRH):
+			s->gpio.CRH = val;
+			break;
+		case offsetof(GPIO_TypeDef, IDR):
+			printf("Error : GPIO forbiden write acces IDR (offset %x), val %lx\n", offset, val);
+			break;
+		case offsetof(GPIO_TypeDef, ODR):
+			s->gpio.ODR = val & 0xFFFF;
+			break;
+		case offsetof(GPIO_TypeDef, BSRR):
+			s->gpio.BSRR = val;
+			break;
+		case offsetof(GPIO_TypeDef, BRR):
+			s->gpio.BRR = val & 0xFFFF;
+			break;
+		case offsetof(GPIO_TypeDef, LCKR):
+			s->gpio.LCKR = val & 0x1FFFF;
+			break;
+		default:
+			printf("Error : GPIO forbiden write acces offset %x, val %lx\n", offset, val);
+			break;
+	}
+}
+
+static uint64_t atlantronic_gpio_read(void *opaque, target_phys_addr_t offset, unsigned size)
+{
+	uint64_t res = 0;
+	struct atlantronic_gpio_state* s = opaque;
+
+	switch(offset)
+	{
+		case offsetof(GPIO_TypeDef, CRL):
+			res = s->gpio.CRL;
+			break;
+		case offsetof(GPIO_TypeDef, CRH):
+			res = s->gpio.CRH;
+			break;
+		case offsetof(GPIO_TypeDef, IDR):
+			res = s->gpio.IDR;
+			break;
+		case offsetof(GPIO_TypeDef, ODR):
+			res = s->gpio.ODR;
+			break;
+		case offsetof(GPIO_TypeDef, BSRR):
+			printf("Error : GPIO BSRR forbiden read acces (offset %x)\n", offset);
+			break;
+		case offsetof(GPIO_TypeDef, BRR):
+			printf("Error : GPIO BRR forbiden read acces (offset %x)\n", offset);
+			break;
+		case offsetof(GPIO_TypeDef, LCKR):
+			res = s->gpio.LCKR;
+			break;
+		default:
+			printf("Error : GPIO forbiden read acces offset %x\n", offset);
+			break;
+	}
+
+	return res;	
+}
+
+static void atlantronic_gpio_reset(GPIO_TypeDef* gpio)
+{
+	gpio->CRL  = 0x44444444;
+	gpio->CRH  = 0x44444444;
+	gpio->IDR  = 0x00;
+	gpio->ODR  = 0x00;
+	gpio->BSRR = 0x00;
+	gpio->BRR  = 0x00;
+	gpio->LCKR = 0x00;
+}
+
+static const MemoryRegionOps atlantronic_gpio_ops =
+{
+	.read = atlantronic_gpio_read,
+	.write = atlantronic_gpio_write,
+	.endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+static void atlantronic_gpio_in_recv(void * opaque, int numPin, int level)
+{
+	if(numPin >= 8 || numPin < 0)
+	{
+		hw_error("gpio it on pin %d - lv %d\n", numPin, level);
+		return;
+	}
+
+    struct atlantronic_gpio_state *s = opaque;
+
+	if(level)
+	{
+		s->gpio.IDR |= (1 << numPin);
+	}
+	else
+	{
+		s->gpio.IDR &= ~(1 << numPin);
+	}
+}
+
+static int atlantronic_gpio_init(SysBusDevice * dev)
+{
+    struct atlantronic_gpio_state *s = FROM_SYSBUS(struct atlantronic_gpio_state, dev);
+
+	memory_region_init_io(&s->iomem, &atlantronic_gpio_ops, s, "atlantronic_gpio", 0x400);
+	sysbus_init_mmio(dev, &s->iomem);
+
+	qdev_init_gpio_out(&dev->qdev, s->irq, PIN_NUM);
+	qdev_init_gpio_in(&dev->qdev, atlantronic_gpio_in_recv, PIN_NUM);
+
+	atlantronic_gpio_reset(&s->gpio);
+
+    return 0;
+}
+
+static void atlantronic_gpio_class_init(ObjectClass *klass, void *data)
+{
+	SysBusDeviceClass *sdc = SYS_BUS_DEVICE_CLASS(klass);
+
+	sdc->init = atlantronic_gpio_init;
+}
+
+static TypeInfo atlantronic_gpio_info =
+{
+	.name          = "atlantronic-gpio",
+	.parent        = TYPE_SYS_BUS_DEVICE,
+	.instance_size = sizeof(struct atlantronic_gpio_state),
+	.class_init    = atlantronic_gpio_class_init,
+};
+
+static void atlantronic_gpio_register_types(void)
+{
+	type_register_static(&atlantronic_gpio_info);
+}
+
+type_init(atlantronic_gpio_register_types);
---
+++ hw/atlantronic_rcc.c
@@ -0,0 +1,243 @@
+#define LINUX
+#include "kernel/cpu/cpu.h"
+#undef LINUX
+#include "kernel/driver/usb/otgd_fs_regs.h"
+
+#include "sysbus.h"
+#include "arm-misc.h"
+#include "boards.h"
+#include "exec-memory.h"
+
+struct atlantronic_rcc_state
+{
+	SysBusDevice busdev;
+	MemoryRegion iomem;
+	RCC_TypeDef rcc;
+};
+
+static void atlantronic_rcc_write_CR(RCC_TypeDef* rcc, uint32_t val)
+{
+	uint64_t write_mask = 0x1509FF01;
+	if(! (rcc->CR & RCC_CR_HSION))
+	{
+		write_mask |= RCC_CR_HSEBYP;
+	}
+
+	rcc->CR = (rcc->CR & ~write_mask) | (val & write_mask);
+
+	if(rcc->CR & RCC_CR_HSION)
+	{
+		// horloge déjà prête
+		rcc->CR |= RCC_CR_HSIRDY;
+	}
+	else
+	{
+		rcc->CR &= ~RCC_CR_HSIRDY;
+	}
+
+	if(rcc->CR & RCC_CR_HSEON)
+	{
+		// horloge déjà prête
+		rcc->CR |= RCC_CR_HSERDY;
+	}
+	else
+	{
+		rcc->CR &= ~RCC_CR_HSERDY;
+	}
+
+	if(rcc->CR & RCC_CR_PLLON)
+	{
+		// PLL prête
+		rcc->CR |= RCC_CR_PLLRDY;
+	}
+	else
+	{
+		rcc->CR &= ~RCC_CR_PLLRDY;
+	}
+
+	if(rcc->CR & RCC_CR_PLL2ON)
+	{
+		// PLL2 prête
+		rcc->CR |= RCC_CR_PLL2RDY;
+	}
+	else
+	{
+		rcc->CR &= ~RCC_CR_PLL2RDY;
+	}
+
+	if(rcc->CR & RCC_CR_PLL3ON)
+	{
+		// PLL3 prête
+		rcc->CR |= RCC_CR_PLL3RDY;
+	}
+	else
+	{
+		rcc->CR &= ~RCC_CR_PLL3RDY;
+	}
+}
+
+static void atlantronic_rcc_write(void *opaque, target_phys_addr_t offset, uint64_t val, unsigned size)
+{
+	struct atlantronic_rcc_state* s = opaque;
+
+	switch(offset)
+	{
+		case offsetof(RCC_TypeDef, CR):
+			atlantronic_rcc_write_CR(&s->rcc, val);
+			break;
+		case offsetof(RCC_TypeDef, CFGR):
+			s->rcc.CFGR = val;
+			if(val & RCC_CFGR_SW_PLL)
+			{
+				// PLL utilisée pour SYSCLK
+				s->rcc.CFGR &= ~RCC_CFGR_SWS;
+				s->rcc.CFGR |= RCC_CFGR_SWS_1;
+			}
+			break;
+		case offsetof(RCC_TypeDef, CIR):
+			s->rcc.CIR = val;
+			break;
+		case offsetof(RCC_TypeDef, APB2RSTR):
+			s->rcc.APB2RSTR = val;
+			break;
+		case offsetof(RCC_TypeDef, APB1RSTR):
+			s->rcc.APB1RSTR = val;
+			break;
+		case offsetof(RCC_TypeDef, AHBENR):
+			s->rcc.AHBENR = val;
+			break;
+		case offsetof(RCC_TypeDef, APB1ENR):
+			s->rcc.APB1ENR = val;
+			break;
+		case offsetof(RCC_TypeDef, APB2ENR):
+			s->rcc.APB2ENR = val;
+			break;
+		case offsetof(RCC_TypeDef, BDCR):
+			s->rcc.BDCR = val;
+			if(val & RCC_BDCR_LSEON)
+			{
+				printf("Error : pas de LSE\n");
+			}
+			break;
+		case offsetof(RCC_TypeDef, CSR):
+			s->rcc.CSR = val;
+			break;
+		case offsetof(RCC_TypeDef, AHBRSTR):
+			s->rcc.AHBRSTR = val;
+			break;
+		case offsetof(RCC_TypeDef, CFGR2):
+			s->rcc.CFGR2 = val;
+			break;
+		default:
+			printf("Error : RCC forbiden write acces offset %x, val %lx\n", offset, val);
+			break;
+	}
+}
+
+static uint64_t atlantronic_rcc_read(void *opaque, target_phys_addr_t offset, unsigned size)
+{
+	uint64_t res = 0;
+	struct atlantronic_rcc_state* s = opaque;
+
+	switch(offset)
+	{
+		case offsetof(RCC_TypeDef, CR):
+			res = s->rcc.CR & (uint32_t)0xFF0FFFFB;
+			break;
+		case offsetof(RCC_TypeDef, CFGR):
+			res = s->rcc.CFGR;
+			break; 
+		case offsetof(RCC_TypeDef, CIR):
+			res = s->rcc.CIR;
+			break;
+		case offsetof(RCC_TypeDef, APB2RSTR):
+			res = s->rcc.APB2RSTR;
+			break;
+		case offsetof(RCC_TypeDef, APB1RSTR):
+			res = s->rcc.APB1RSTR;
+			break;
+		case offsetof(RCC_TypeDef, AHBENR):
+			res = s->rcc.AHBENR;
+			break;
+		case offsetof(RCC_TypeDef, APB1ENR):
+			res = s->rcc.APB1ENR;
+			break;
+		case offsetof(RCC_TypeDef, APB2ENR):
+			res = s->rcc.APB2ENR;
+			break;
+		case offsetof(RCC_TypeDef, BDCR):
+			res = s->rcc.BDCR;
+			break;
+		case offsetof(RCC_TypeDef, CSR):
+			res = s->rcc.CSR;
+			break;
+		case offsetof(RCC_TypeDef, AHBRSTR):
+			res = s->rcc.AHBRSTR;
+			break;
+		case offsetof(RCC_TypeDef, CFGR2):
+			res = s->rcc.CFGR2;
+			break;
+		default:
+			printf("Error : RCC forbiden read acces offset %x\n", offset);
+			break;
+	}
+
+	return res;	
+}
+
+static void atlantronic_rcc_reset(RCC_TypeDef* rcc)
+{
+	rcc->CR = 0x00000083;
+	rcc->CFGR = 0x00;
+	rcc->CIR = 0x00;
+	rcc->APB2RSTR = 0x00;
+	rcc->APB1RSTR = 0x00;
+	rcc->AHBENR = 0x14;
+	rcc->APB2ENR = 0x00;
+	rcc->APB1ENR = 0x00;
+	rcc->BDCR = 0x00;
+	rcc->CSR = 0x0C000000;
+	rcc->AHBRSTR = 0x00;
+	rcc->CFGR2 = 0x00;
+}
+
+static const MemoryRegionOps atlantronic_rcc_ops =
+{
+	.read = atlantronic_rcc_read,
+	.write = atlantronic_rcc_write,
+	.endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+static int atlantronic_rcc_init(SysBusDevice * dev)
+{
+    struct atlantronic_rcc_state *s = FROM_SYSBUS(struct atlantronic_rcc_state, dev);
+
+	memory_region_init_io(&s->iomem, &atlantronic_rcc_ops, s, "atlantronic_rcc", 0x400);
+	sysbus_init_mmio(dev, &s->iomem);
+
+	atlantronic_rcc_reset(&s->rcc);
+
+    return 0;
+}
+
+static void atlantronic_rcc_class_init(ObjectClass *klass, void *data)
+{
+	SysBusDeviceClass *sdc = SYS_BUS_DEVICE_CLASS(klass);
+
+	sdc->init = atlantronic_rcc_init;
+}
+
+static TypeInfo atlantronic_rcc_info =
+{
+	.name          = "atlantronic-rcc",
+	.parent        = TYPE_SYS_BUS_DEVICE,
+	.instance_size = sizeof(struct atlantronic_rcc_state),
+	.class_init    = atlantronic_rcc_class_init,
+};
+
+static void atlantronic_rcc_register_types(void)
+{
+	type_register_static(&atlantronic_rcc_info);
+}
+
+type_init(atlantronic_rcc_register_types);
