d04e5a9ceb5d73eb1cce970ecfa0d013fa7198e4
Tredez Jean-Baptiste <jbtredez@gmail.com>
2013-09-15 15:41:56 +0200

    merge atlantronic

---
+++ hw/arm/atlantronic_dma_chan.c
@@ -0,0 +1,198 @@
+#include "hw/sysbus.h"
+#include "hw/boards.h"
+#include "hw/arm/arm.h"
+
+#define LINUX
+#define STM32F10X_CL
+#undef FALSE
+#undef TRUE
+#undef bool
+#include "kernel/cpu/cpu.h"
+#undef LINUX
+
+struct atlantronic_dma_chan_state
+{
+	SysBusDevice busdev;
+	MemoryRegion iomem;
+	MemoryRegion ram;
+	DMA_Channel_TypeDef dma_chan;
+	uint32_t cndtr_init;
+	uint32_t cpar_init;
+	uint32_t cmar_init;
+	qemu_irq irq;
+};
+
+static void atlantronic_update_dma(struct atlantronic_dma_chan_state *s)
+{
+	if( ! s->dma_chan.CCR & 0x01 )
+	{
+		// dma desactive
+		return;
+	}
+
+	if( ! s->dma_chan.CNDTR )
+	{
+		// il n'y a plus rien a copier
+		return;
+	}
+
+	int dir = (s->dma_chan.CCR >> 4) & 0x01;
+	int pinc = (s->dma_chan.CCR >> 6) & 0x01;
+	int minc = (s->dma_chan.CCR >> 7) & 0x01;
+	int psize = 1 << ((s->dma_chan.CCR >> 8) & 0x3);
+	int msize = 1 << ((s->dma_chan.CCR >> 10) & 0x3);
+
+	int64_t val;
+	if( dir )
+	{
+		// sens mem -> perif
+		cpu_physical_memory_read(s->dma_chan.CMAR, &val, psize);
+		cpu_physical_memory_write(s->dma_chan.CPAR, &val, msize);
+	}
+	else
+	{
+		// sens perif -> mem
+		cpu_physical_memory_read(s->dma_chan.CPAR, &val, psize);
+		cpu_physical_memory_write(s->dma_chan.CMAR, &val, msize);
+	}
+
+	s->dma_chan.CPAR += psize * pinc;
+	s->dma_chan.CMAR += msize * minc;
+	s->dma_chan.CNDTR--;
+	if( ! s->dma_chan.CNDTR )
+	{
+		s->dma_chan.CPAR = s->cpar_init;
+		s->dma_chan.CMAR = s->cmar_init;
+		// si circ : rechargement cndtr
+		if( ((s->dma_chan.CCR >> 5) & 0x01 ) && ! dir )
+		{
+			s->dma_chan.CNDTR = s->cndtr_init;
+		}
+
+		// IRQ fin de transfert
+		qemu_set_irq(s->irq, 2);
+	}
+}
+
+static void atlantronic_dma_chan_write(void *opaque, hwaddr offset, uint64_t val, unsigned size)
+{
+	struct atlantronic_dma_chan_state* s = opaque;
+
+	switch(offset)
+	{
+		case offsetof(DMA_Channel_TypeDef, CCR):
+			if( ! (s->dma_chan.CCR & 0x01) && (val & 0x01) )
+			{
+				s->cndtr_init = s->dma_chan.CNDTR;
+				s->cpar_init = s->dma_chan.CPAR;
+				s->cmar_init = s->dma_chan.CMAR;
+				if( (s->dma_chan.CCR >> 4) & 0x01 )
+				{
+					// mem -> perif
+					while( s->dma_chan.CNDTR )
+					{
+						atlantronic_update_dma(s);
+					}
+				}
+			}			
+			s->dma_chan.CCR = val;
+			break;
+		case offsetof(DMA_Channel_TypeDef, CNDTR):
+			s->dma_chan.CNDTR = val;
+			break;
+		case offsetof(DMA_Channel_TypeDef, CPAR):
+			s->dma_chan.CPAR = val & 0xfffffffe;
+			break;
+		case offsetof(DMA_Channel_TypeDef, CMAR):
+			s->dma_chan.CMAR = val & 0xfffffffe;
+			break;
+		default:
+			printf("Error : DMA Channel forbiden write acces offset %lx, val %lx\n", offset, val);
+			break;
+	}
+}
+
+static uint64_t atlantronic_dma_chan_read(void *opaque, hwaddr offset, unsigned size)
+{
+	uint64_t res = 0;
+	struct atlantronic_dma_chan_state* s = opaque;
+
+	switch(offset)
+	{
+		case offsetof(DMA_Channel_TypeDef, CCR):
+			res = s->dma_chan.CCR;
+			break;
+		case offsetof(DMA_Channel_TypeDef, CNDTR):
+			res = s->dma_chan.CNDTR;
+			break;
+		case offsetof(DMA_Channel_TypeDef, CPAR):
+			res = s->dma_chan.CPAR;
+			break;
+		case offsetof(DMA_Channel_TypeDef, CMAR):
+			res = s->dma_chan.CMAR;
+			break;
+		default:
+			printf("Error : DMA Channel forbiden read acces offset %lx\n", offset);
+			break;
+	}
+
+	return res;	
+}
+
+static void atlantronic_dma_chan_reset(DMA_Channel_TypeDef* chan)
+{
+	chan->CCR = 0x00;
+	chan->CNDTR = 0x00;
+	chan->CPAR = 0x00;
+	chan->CMAR = 0x00;
+}
+
+static const MemoryRegionOps atlantronic_dma_chan_ops =
+{
+	.read = atlantronic_dma_chan_read,
+	.write = atlantronic_dma_chan_write,
+	.endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+static void atlantronic_dma_chan_in_recv(void * opaque, int numPin, int level)
+{
+	struct atlantronic_dma_chan_state *s = opaque;
+	atlantronic_update_dma(s);
+}
+
+static int atlantronic_dma_chan_init(SysBusDevice * dev)
+{
+	struct atlantronic_dma_chan_state *s = OBJECT_CHECK(struct atlantronic_dma_chan_state, dev, "atlantronic-dma-chan");
+
+	memory_region_init_io(&s->iomem, OBJECT(s), &atlantronic_dma_chan_ops, s, "atlantronic_dma_chan", 0x010);
+	sysbus_init_mmio(dev, &s->iomem);
+
+	qdev_init_gpio_out(DEVICE(dev), &s->irq, 1);
+	qdev_init_gpio_in(DEVICE(dev), atlantronic_dma_chan_in_recv, 1);
+
+	atlantronic_dma_chan_reset(&s->dma_chan);
+
+    return 0;
+}
+
+static void atlantronic_dma_chan_class_init(ObjectClass *klass, void *data)
+{
+	SysBusDeviceClass *sdc = SYS_BUS_DEVICE_CLASS(klass);
+
+	sdc->init = atlantronic_dma_chan_init;
+}
+
+static TypeInfo atlantronic_dma_chan_info =
+{
+	.name          = "atlantronic-dma-chan",
+	.parent        = TYPE_SYS_BUS_DEVICE,
+	.instance_size = sizeof(struct atlantronic_dma_chan_state),
+	.class_init    = atlantronic_dma_chan_class_init,
+};
+
+static void atlantronic_dma_chan_register_types(void)
+{
+	type_register_static(&atlantronic_dma_chan_info);
+}
+
+type_init(atlantronic_dma_chan_register_types);
---
+++ hw/arm/atlantronic_gpio.c
@@ -0,0 +1,215 @@
+#include "hw/sysbus.h"
+#include "hw/boards.h"
+#include "hw/arm/arm.h"
+
+#define LINUX
+#define STM32F10X_CL
+#undef FALSE
+#undef TRUE
+#undef bool
+#include "kernel/cpu/cpu.h"
+#undef LINUX
+
+#define PIN_NUM        16
+
+struct atlantronic_gpio_state
+{
+	SysBusDevice busdev;
+	MemoryRegion iomem;
+	GPIO_TypeDef gpio;
+	qemu_irq irq[PIN_NUM];
+};
+
+static void atlantronic_gpio_write(void *opaque, hwaddr offset, uint64_t val, unsigned size)
+{
+	struct atlantronic_gpio_state* s = opaque;
+	int32_t diff = 0;
+	int i = 0;
+
+	switch(offset)
+	{
+		case offsetof(GPIO_TypeDef, CRL):
+			s->gpio.CRL = val;
+			break;
+		case offsetof(GPIO_TypeDef, CRH):
+			s->gpio.CRH = val;
+			break;
+		case offsetof(GPIO_TypeDef, IDR):
+			printf("Error : GPIO forbiden write acces IDR (offset %lx), val %lx\n", offset, val);
+			break;
+		case offsetof(GPIO_TypeDef, ODR):
+			val &= 0xFFFF;
+			diff = s->gpio.ODR ^ val;  
+			s->gpio.ODR = val;
+			for( i = 0; i < PIN_NUM; i++)
+			{
+				int mode = 0;
+
+				if( i < 8)
+				{
+					// pin 0 a 7
+					mode = (s->gpio.CRL >> (4 * i)) & 0x03;
+				}
+				else
+				{
+					// pin 8 a 15
+					mode = (s->gpio.CRH >> (4 * (i - 8))) & 0x03;
+				}
+
+				if( mode != 0 && ((diff >> i) & 0x01))
+				{
+					qemu_set_irq(s->irq[i], (val >> i) & 0x01);
+				}
+			}
+			break;
+		case offsetof(GPIO_TypeDef, BSRR):
+			s->gpio.BSRR = val;
+			break;
+		case offsetof(GPIO_TypeDef, BRR):
+			s->gpio.BRR = val & 0xFFFF;
+			break;
+		case offsetof(GPIO_TypeDef, LCKR):
+			s->gpio.LCKR = val & 0x1FFFF;
+			break;
+		default:
+			printf("Error : GPIO forbiden write acces offset %lx, val %lx\n", offset, val);
+			break;
+	}
+}
+
+static uint64_t atlantronic_gpio_read(void *opaque, hwaddr offset, unsigned size)
+{
+	uint64_t res = 0;
+	struct atlantronic_gpio_state* s = opaque;
+
+	switch(offset)
+	{
+		case offsetof(GPIO_TypeDef, CRL):
+			res = s->gpio.CRL;
+			break;
+		case offsetof(GPIO_TypeDef, CRH):
+			res = s->gpio.CRH;
+			break;
+		case offsetof(GPIO_TypeDef, IDR):
+			res = s->gpio.IDR;
+			break;
+		case offsetof(GPIO_TypeDef, ODR):
+			res = s->gpio.ODR;
+			break;
+		case offsetof(GPIO_TypeDef, BSRR):
+			printf("Error : GPIO BSRR forbiden read acces (offset %lx)\n", offset);
+			break;
+		case offsetof(GPIO_TypeDef, BRR):
+			printf("Error : GPIO BRR forbiden read acces (offset %lx)\n", offset);
+			break;
+		case offsetof(GPIO_TypeDef, LCKR):
+			res = s->gpio.LCKR;
+			break;
+		default:
+			printf("Error : GPIO forbiden read acces offset %lx\n", offset);
+			break;
+	}
+
+	return res;	
+}
+
+static void atlantronic_gpio_reset(GPIO_TypeDef* gpio)
+{
+	gpio->CRL  = 0x44444444;
+	gpio->CRH  = 0x44444444;
+	gpio->IDR  = 0x00;
+	gpio->ODR  = 0x00;
+	gpio->BSRR = 0x00;
+	gpio->BRR  = 0x00;
+	gpio->LCKR = 0x00;
+}
+
+static const MemoryRegionOps atlantronic_gpio_ops =
+{
+	.read = atlantronic_gpio_read,
+	.write = atlantronic_gpio_write,
+	.endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+static void atlantronic_gpio_in_recv(void * opaque, int numPin, int level)
+{
+	struct atlantronic_gpio_state *s = opaque;
+
+	int mode = 0;
+	int cnf = 0;
+
+	if( numPin < 8)
+	{
+		// pin 0 a 7
+		mode = (s->gpio.CRL >> (4 * numPin)) & 0x03;
+		cnf  = (s->gpio.CRL >> (4 * numPin + 2)) & 0x03;
+	}
+	else
+	{
+		// pin 8 a 15
+		mode = (s->gpio.CRH >> (4 * (numPin - 8))) & 0x03;
+		cnf  = (s->gpio.CRH >> (4 * (numPin - 8) + 2)) & 0x03;
+	}
+
+	if( mode != 0 )
+	{
+		// pin configurée en sortie, erreur
+		hw_error("conflict, try to set gpio output pin %d lv %d", numPin, level);
+		return;
+	}
+
+	if( cnf != 0 )
+	{
+		// io 0 / 1
+		if(level)
+		{
+			s->gpio.IDR |= (1 << numPin);
+		}
+		else
+		{
+			s->gpio.IDR &= ~(1 << numPin);
+		}
+	}
+	else
+	{
+		// AN, envoi vers le module adc
+		qemu_set_irq(s->irq[numPin], level);
+	}
+}
+
+static int atlantronic_gpio_init(SysBusDevice * dev)
+{
+	struct atlantronic_gpio_state *s = OBJECT_CHECK(struct atlantronic_gpio_state, dev, "atlantronic-gpio");
+
+	memory_region_init_io(&s->iomem, OBJECT(s), &atlantronic_gpio_ops, s, "atlantronic_gpio", 0x400);
+	sysbus_init_mmio(dev, &s->iomem);
+
+	qdev_init_gpio_out(DEVICE(dev), s->irq, PIN_NUM);
+	qdev_init_gpio_in(DEVICE(dev), atlantronic_gpio_in_recv, PIN_NUM);
+
+	atlantronic_gpio_reset(&s->gpio);
+
+	return 0;
+}
+
+static void atlantronic_gpio_class_init(ObjectClass *klass, void *data)
+{
+	SysBusDeviceClass *sdc = SYS_BUS_DEVICE_CLASS(klass);
+
+	sdc->init = atlantronic_gpio_init;
+}
+
+static TypeInfo atlantronic_gpio_info =
+{
+	.name          = "atlantronic-gpio",
+	.parent        = TYPE_SYS_BUS_DEVICE,
+	.instance_size = sizeof(struct atlantronic_gpio_state),
+	.class_init    = atlantronic_gpio_class_init,
+};
+
+static void atlantronic_gpio_register_types(void)
+{
+	type_register_static(&atlantronic_gpio_info);
+}
+
+type_init(atlantronic_gpio_register_types);
