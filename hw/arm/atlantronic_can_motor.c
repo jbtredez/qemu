2314d296b819f068d1da497a058256ca0f7a093d
Tredez Jean-Baptiste <jbtredez@gmail.com>
2013-10-16 10:37:53 +0200

    can - sdo moteurs ok

--- hw/arm/atlantronic_can_motor.c
+++ hw/arm/atlantronic_can_motor.c
@@ -1,13 +1,29 @@
 #include "atlantronic_can_motor.h"
 #include "atlantronic_canopen.h"
 
-void atlantronic_can_motor_callback(void* motor, struct can_msg msg, int type);
+void atlantronic_can_motor_callback(void* can_interface, void* opaque, struct can_msg msg, int type);
 
-void atlantronic_can_motor_callback(void* motor, struct can_msg msg, int type)
+void atlantronic_can_motor_callback(void* can_interface, void* opaque, struct can_msg msg, int type)
 {
-	(void) motor;
-	(void) msg;
-	(void) type;
+	struct atlantronic_can_motor* motor = opaque;
+	struct can_msg rx_msg;
+
+	switch(type)
+	{
+		case CANOPEN_SDO_RX:
+			// on repond ok a tout les sdo
+			rx_msg.id = CANOPEN_SDO_RES + motor->nodeid;
+			rx_msg.data[0] = 60;
+			rx_msg.data[1] = msg.data[1];
+			rx_msg.data[2] = msg.data[2];
+			rx_msg.data[3] = msg.data[3];
+			rx_msg.size = 4;
+
+			atlantronic_can_rx(can_interface, rx_msg);
+			break;
+		default:
+			break;
+	}
 }
 
 int atlantronic_can_motor_connect(struct atlantronic_can_motor* motor)
