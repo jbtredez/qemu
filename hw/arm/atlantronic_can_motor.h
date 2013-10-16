6c1987182adf0c1cfc3a17fa879447f92fae2c6a
Tredez Jean-Baptiste <jbtredez@gmail.com>
2013-10-16 09:34:45 +0200

    gestion can/canopen + can_motor. bootup ok

---
+++ hw/arm/atlantronic_can_motor.h
@@ -0,0 +1,17 @@
+#ifndef ATLANTRONIC_CAN_MOTOR_H
+#define ATLANTRONIC_CAN_MOTOR_H
+
+#include <stdint.h>
+
+struct atlantronic_can_motor
+{
+	uint8_t nodeid; //! id
+	float posCmd;    //! commande de position
+	float speedCmd;  //! commande de vitesse
+	float pos;       //! position actuelle
+	float v;         //! vitesse actuelle
+};
+
+int atlantronic_can_motor_connect(struct atlantronic_can_motor* motor);
+
+#endif
