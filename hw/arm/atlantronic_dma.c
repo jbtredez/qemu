6a0afba60c24d01c0072287a61f9528758208543
Tredez Jean-Baptiste <jbtredez@gmail.com>
2013-12-18 23:20:27 +0100

    dma

--- hw/arm/atlantronic_dma.c
+++ hw/arm/atlantronic_dma.c
@@ -3,45 +3,49 @@
 #include "hw/arm/arm.h"
 #include "atlantronic_cpu.h"
 
-#define DMA_CHAN_NUMBER                7
+#define DMA_STREAM_NUMBER                7
 
 struct atlantronic_dma_state
 {
 	SysBusDevice busdev;
 	MemoryRegion iomem;
 	DMA_TypeDef dma;
-	qemu_irq irq[DMA_CHAN_NUMBER];
+	qemu_irq irq[DMA_STREAM_NUMBER];
 };
+
+static void atlantronic_dma_write_ifcr(volatile uint32_t* xISR, uint32_t val, int bitNum, qemu_irq* irq)
+{
+	uint32_t v = (val >> bitNum) & 0x3f;
+	*xISR &= ~(v << bitNum );
+	if( v & 0x30 )
+	{
+		// TCIF ou HTIF
+		// => declencher it
+		*xISR &= ~(0x3f << bitNum );
+		qemu_set_irq(*irq, 0);
+	}
+}
 
 static void atlantronic_dma_write(void *opaque, hwaddr offset, uint64_t val, unsigned size)
 {
 	struct atlantronic_dma_state* s = opaque;
-	int i = 0;
 
 	switch(offset)
 	{
-		case offsetof(DMA_TypeDef, IFCR):
-			for(i = 0; i < DMA_CHAN_NUMBER; i++)
-			{
-				int32_t v = val & 0x0f;
-				if( v & 0x1)
-				{
-					// clear complet des flags du chan i
-					s->dma.ISR &= ~(0x0f << (4 * i) );
-					qemu_set_irq(s->irq[i], 0);
-				}
-				else
-				{
-					s->dma.ISR &= ~((v & 0x0f) << (4 * i) );
-					if( v & 0x02 )
-					{
-						qemu_set_irq(s->irq[i], 0);
-					}
-				}
-				val >>= 4;
-			}
+		case offsetof(DMA_TypeDef, LIFCR):
+			atlantronic_dma_write_ifcr(&s->dma.LISR, val,  0, &s->irq[0]);
+			atlantronic_dma_write_ifcr(&s->dma.LISR, val,  6, &s->irq[1]);
+			atlantronic_dma_write_ifcr(&s->dma.LISR, val, 16, &s->irq[2]);
+			atlantronic_dma_write_ifcr(&s->dma.LISR, val, 22, &s->irq[3]);
 			break;
-		case offsetof(DMA_TypeDef, ISR):
+		case offsetof(DMA_TypeDef, HIFCR):
+			atlantronic_dma_write_ifcr(&s->dma.HISR, val,  0, &s->irq[4]);
+			atlantronic_dma_write_ifcr(&s->dma.HISR, val,  6, &s->irq[5]);
+			atlantronic_dma_write_ifcr(&s->dma.HISR, val, 16, &s->irq[6]);
+			atlantronic_dma_write_ifcr(&s->dma.HISR, val, 22, &s->irq[7]);
+			break;
+		case offsetof(DMA_TypeDef, LISR):
+		case offsetof(DMA_TypeDef, HISR):
 		default:
 			printf("Error : DMA forbiden write acces offset %lx, val %lx\n", offset, val);
 			break;
@@ -55,11 +59,17 @@ static uint64_t atlantronic_dma_read(void *opaque, hwaddr offset, unsigned size)
 
 	switch(offset)
 	{
-		case offsetof(DMA_TypeDef, ISR):
-			res = s->dma.ISR;
+		case offsetof(DMA_TypeDef, LISR):
+			res = s->dma.LISR;
 			break;
-		case offsetof(DMA_TypeDef, IFCR):
-			res = s->dma.IFCR;
+		case offsetof(DMA_TypeDef, LIFCR):
+			res = s->dma.LIFCR;
+			break;
+		case offsetof(DMA_TypeDef, HISR):
+			res = s->dma.HISR;
+			break;
+		case offsetof(DMA_TypeDef, HIFCR):
+			res = s->dma.HIFCR;
 			break;
 		default:
 			printf("Error : DMA forbiden read acces offset %lx\n", offset);
@@ -71,8 +81,10 @@ static uint64_t atlantronic_dma_read(void *opaque, hwaddr offset, unsigned size)
 
 static void atlantronic_dma_reset(DMA_TypeDef* dma)
 {
-	dma->ISR = 0x00;
-	dma->IFCR = 0x00;
+	dma->LISR = 0x00;
+	dma->LIFCR = 0x00;
+	dma->HISR = 0x00;
+	dma->HIFCR = 0x00;
 }
 
 static const MemoryRegionOps atlantronic_dma_ops =
@@ -86,28 +98,67 @@ static void atlantronic_dma_in_recv(void * opaque, int numPin, int level)
 {
 	struct atlantronic_dma_state *s = opaque;
 
-	level &= 0x0f;
-	uint32_t old_val = (s->dma.ISR >> (4 * numPin)) & 0x0f;
-	s->dma.ISR |= level << (4 * numPin);
+	level &= 0x03f;
+	uint32_t old_val;
+	int bitShift = 0;
+
+	volatile uint32_t* xISR = &s->dma.LISR;
+	if(level >= 4)
+	{
+		xISR = &s->dma.HISR;
+	}
+
+	switch(level)
+	{
+		case 0:
+			bitShift = 0;
+			break;
+		case 1:
+			bitShift = 6;
+			break;
+		case 2:
+			bitShift = 16;
+			break;
+		case 3:
+			bitShift = 22;
+			break;
+		case 4:
+			bitShift = 0;
+			break;
+		case 5:
+			bitShift = 6;
+			break;
+		case 6:
+			bitShift = 16;
+			break;
+		case 7:
+			bitShift = 22;
+			break;
+		default:
+			return;
+	}
+
+
+	old_val = (*xISR >> bitShift) & 0x03f;
+	*xISR |= level << bitShift;
 
 	uint32_t diff_raise = (old_val ^ level) & level;
-	if( diff_raise & 0x02 )
+	// TODO voir / autres irq (erreur, half transfert)
+	if( diff_raise & 0x20 )
 	{
 		qemu_set_irq(s->irq[numPin], 1);
 	}
-
-	// TODO voir / autres irq (erreur et half transfert)
 }
 
 static int atlantronic_dma_init(SysBusDevice * dev)
 {
 	struct atlantronic_dma_state *s = OBJECT_CHECK(struct atlantronic_dma_state, dev, "atlantronic-dma");
 
-	memory_region_init_io(&s->iomem, OBJECT(s), &atlantronic_dma_ops, s, "atlantronic_dma", 0x08);
+	memory_region_init_io(&s->iomem, OBJECT(s), &atlantronic_dma_ops, s, "atlantronic_dma", sizeof(DMA_TypeDef));
 	sysbus_init_mmio(dev, &s->iomem);
 
-	qdev_init_gpio_out(DEVICE(dev), s->irq, DMA_CHAN_NUMBER);
-	qdev_init_gpio_in(DEVICE(dev), atlantronic_dma_in_recv, DMA_CHAN_NUMBER);
+	qdev_init_gpio_out(DEVICE(dev), s->irq, DMA_STREAM_NUMBER);
+	qdev_init_gpio_in(DEVICE(dev), atlantronic_dma_in_recv, DMA_STREAM_NUMBER);
 
 	atlantronic_dma_reset(&s->dma);
 
---
+++ hw/arm/atlantronic_dma_stream.c
@@ -0,0 +1,191 @@
+#include "hw/sysbus.h"
+#include "hw/boards.h"
+#include "hw/arm/arm.h"
+#include "atlantronic_cpu.h"
+
+struct atlantronic_dma_stream_state
+{
+	SysBusDevice busdev;
+	MemoryRegion iomem;
+	MemoryRegion ram;
+	DMA_Stream_TypeDef dma_stream;
+	uint32_t cndtr_init;
+	uint32_t cpar_init;
+	uint32_t cmar_init;
+	qemu_irq irq;
+};
+
+static void atlantronic_update_dma(struct atlantronic_dma_stream_state *s)
+{
+	if( ! s->dma_stream.CR & 0x01 )
+	{
+		// dma desactive
+		return;
+	}
+
+	if( ! s->dma_stream.NDTR )
+	{
+		// il n'y a plus rien a copier
+		return;
+	}
+
+	int dir = (s->dma_stream.CR >> 6) & 0x02;
+	int pinc = (s->dma_stream.CR >> 9) & 0x01;
+	int minc = (s->dma_stream.CR >> 10) & 0x01;
+	int psize = 1 << ((s->dma_stream.CR >> 11) & 0x3);
+	int msize = 1 << ((s->dma_stream.CR >> 13) & 0x3);
+
+	int64_t val;
+	if( dir == 0x01 )
+	{
+		// sens mem -> perif
+		cpu_physical_memory_read(s->dma_stream.M0AR, &val, psize);
+		cpu_physical_memory_write(s->dma_stream.PAR, &val, msize);
+	}
+	else if( dir == 0x00 )
+	{
+		// sens perif -> mem
+		cpu_physical_memory_read(s->dma_stream.PAR, &val, psize);
+		cpu_physical_memory_write(s->dma_stream.M0AR, &val, msize);
+	}
+
+	s->dma_stream.PAR += psize * pinc;
+	s->dma_stream.M0AR += msize * minc;
+	s->dma_stream.NDTR--;
+	if( ! s->dma_stream.NDTR )
+	{
+		s->dma_stream.PAR = s->cpar_init;
+		s->dma_stream.M0AR = s->cmar_init;
+		// si circ : rechargement cndtr
+		if( ((s->dma_stream.CR >> 5) & 0x01 ) && ! dir )
+		{
+			s->dma_stream.NDTR = s->cndtr_init;
+		}
+
+		// IRQ fin de transfert : TCIF
+		qemu_set_irq(s->irq, 0x20);
+	}
+}
+
+static void atlantronic_dma_stream_write(void *opaque, hwaddr offset, uint64_t val, unsigned size)
+{
+	struct atlantronic_dma_stream_state* s = opaque;
+
+	switch(offset)
+	{
+		case offsetof(DMA_Stream_TypeDef, CR):
+			if( ! (s->dma_stream.CR & 0x01) && (val & 0x01) )
+			{
+				s->cndtr_init = s->dma_stream.NDTR;
+				s->cpar_init = s->dma_stream.PAR;
+				s->cmar_init = s->dma_stream.M0AR;
+				if( (s->dma_stream.CR >> 4) & 0x01 )
+				{
+					// mem -> perif
+					while( s->dma_stream.NDTR )
+					{
+						atlantronic_update_dma(s);
+					}
+				}
+			}			
+			s->dma_stream.CR = val;
+			break;
+		case offsetof(DMA_Stream_TypeDef, NDTR):
+			s->dma_stream.NDTR = val;
+			break;
+		case offsetof(DMA_Stream_TypeDef, PAR):
+			s->dma_stream.PAR = val & 0xfffffffe;
+			break;
+		case offsetof(DMA_Stream_TypeDef, M0AR):
+			s->dma_stream.M0AR = val & 0xfffffffe;
+			break;
+		default:
+			printf("Error : DMA Channel forbiden write acces offset %lx, val %lx\n", offset, val);
+			break;
+	}
+}
+
+static uint64_t atlantronic_dma_stream_read(void *opaque, hwaddr offset, unsigned size)
+{
+	uint64_t res = 0;
+	struct atlantronic_dma_stream_state* s = opaque;
+
+	switch(offset)
+	{
+		case offsetof(DMA_Stream_TypeDef, CR):
+			res = s->dma_stream.CR;
+			break;
+		case offsetof(DMA_Stream_TypeDef, NDTR):
+			res = s->dma_stream.NDTR;
+			break;
+		case offsetof(DMA_Stream_TypeDef, PAR):
+			res = s->dma_stream.PAR;
+			break;
+		case offsetof(DMA_Stream_TypeDef, M0AR):
+			res = s->dma_stream.M0AR;
+			break;
+		default:
+			printf("Error : DMA Channel forbiden read acces offset %lx\n", offset);
+			break;
+	}
+
+	return res;	
+}
+
+static void atlantronic_dma_stream_reset(DMA_Stream_TypeDef* chan)
+{
+	chan->CR = 0x00;
+	chan->NDTR = 0x00;
+	chan->PAR = 0x00;
+	chan->M0AR = 0x00;
+}
+
+static const MemoryRegionOps atlantronic_dma_stream_ops =
+{
+	.read = atlantronic_dma_stream_read,
+	.write = atlantronic_dma_stream_write,
+	.endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+static void atlantronic_dma_stream_in_recv(void * opaque, int numPin, int level)
+{
+	struct atlantronic_dma_stream_state *s = opaque;
+	atlantronic_update_dma(s);
+}
+
+static int atlantronic_dma_stream_init(SysBusDevice * dev)
+{
+	struct atlantronic_dma_stream_state *s = OBJECT_CHECK(struct atlantronic_dma_stream_state, dev, "atlantronic-dma-stream");
+
+	memory_region_init_io(&s->iomem, OBJECT(s), &atlantronic_dma_stream_ops, s, "atlantronic_dma_stream", sizeof(DMA_Stream_TypeDef));
+	sysbus_init_mmio(dev, &s->iomem);
+
+	qdev_init_gpio_out(DEVICE(dev), &s->irq, 1);
+	qdev_init_gpio_in(DEVICE(dev), atlantronic_dma_stream_in_recv, 1);
+
+	atlantronic_dma_stream_reset(&s->dma_stream);
+
+    return 0;
+}
+
+static void atlantronic_dma_stream_class_init(ObjectClass *klass, void *data)
+{
+	SysBusDeviceClass *sdc = SYS_BUS_DEVICE_CLASS(klass);
+
+	sdc->init = atlantronic_dma_stream_init;
+}
+
+static TypeInfo atlantronic_dma_stream_info =
+{
+	.name          = "atlantronic-dma-stream",
+	.parent        = TYPE_SYS_BUS_DEVICE,
+	.instance_size = sizeof(struct atlantronic_dma_stream_state),
+	.class_init    = atlantronic_dma_stream_class_init,
+};
+
+static void atlantronic_dma_stream_register_types(void)
+{
+	type_register_static(&atlantronic_dma_stream_info);
+}
+
+type_init(atlantronic_dma_stream_register_types);
