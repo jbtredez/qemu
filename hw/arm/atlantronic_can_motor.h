b7d996c2316eed0515a817e247e8c9bfe13703b5
Tredez Jean-Baptiste <jbtredez@gmail.com>
2013-12-30 13:26:43 +0100

    modele - mise a jour de la position du robot

--- hw/arm/atlantronic_can_motor.h
+++ hw/arm/atlantronic_can_motor.h
@@ -4,14 +4,25 @@
 #include <stdint.h>
 #include "atlantronic_canopen.h"
 
+#define MOTOR_ENCODER_RESOLUTION         3000
+
+
 struct atlantronic_can_motor
 {
 	struct canopen_node node;
 	int32_t speedCmd;  //! commande de vitesse en rpm
-	uint32_t pos;      //! position actuelle
-	//float v;           //! vitesse actuelle
+	float raw_pos;     //! position actuelle
+	float raw_v;       //! vitesse actuelle
+	float pos;         //! position actuelle
+	float v;           //! vitesse actuelle
+	float dtSync;      //!< temps passe depuis le dernier sync
+	float outputGain;  //!< gain sur la sortie pour avoir la position et la vitesse en unités utilisables (rd/s ou mm/s)
 };
 
+void atlantronic_can_motor_init(struct atlantronic_can_motor* s, float outputGain);
+
 void atlantronic_can_motor_callback(struct atlantronic_canopen* canopen, struct canopen_node* node, struct can_msg msg, int type);
+
+void atlantronic_can_motor_update(struct atlantronic_can_motor* s, float dt);
 
 #endif
--- hw/arm/atlantronic_hokuyo.h
+++ hw/arm/atlantronic_hokuyo.h
@@ -4,6 +4,7 @@
 #define HOKUYO_MAX_MES                 769
 
 #include "hw/irq.h"
+#include "atlantronic_tools.h"
 
 struct atlantronic_hokuyo_state
 {
@@ -17,15 +18,14 @@ struct atlantronic_hokuyo_state
 	int scan_ready;
 	int send_scan_when_ready;
 	uint64_t timer_count;
-	float x;
-	float y;
-	float theta;
+	struct atlantronic_vect3 pos_robot;   //!< poosition du robot
+	struct atlantronic_vect3 pos_hokuyo;  //!< position du hokuyo dans le repere robot
 	int clock_scale;
 };
 
 void atlantronic_hokuyo_in_recv_usart(struct atlantronic_hokuyo_state *s, unsigned char data);
 
-int atlantronic_hokuyo_init(struct atlantronic_hokuyo_state *s, qemu_irq* irq_tx);
+int atlantronic_hokuyo_init(struct atlantronic_hokuyo_state *s, qemu_irq* irq_tx, struct atlantronic_vect3 pos_hokuyo);
 
 enum
 {
